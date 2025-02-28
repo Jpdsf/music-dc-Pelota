3a8e9aad1c458407a909b0862949d37e3d78b46d
jpdsf <jpdsafg@gmail.com>
2025-02-27 23:18:41 -0300

    First v pelota v.002

--- Pelota/pelota.py
+++ Pelota/pelota.py
@@ -7,21 +7,24 @@ from dotenv import load_dotenv
 def run_bot():
     load_dotenv()
     TOKEN = os.getenv('TOKEN')
+    
     intents = discord.Intents.default()
-    intents.message_content= True
+    intents.message_content = True
     client = discord.Client(intents=intents)
 
-    voice_clients = {
+    voice_clients = {}
 
-    }
     yt_dl_options = {
         "format": "bestaudio/best",
-    } 
+        "cookies": "cookies.txt",  
+        "noplaylist": True,        
+        "quiet": True,             
+    }
     ytdl = yt_dlp.YoutubeDL(yt_dl_options)
 
     ffmpeg_options = {
         'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-        'options': '-vn',
+        'options': '-vn',  
     }
 
     @client.event
@@ -30,24 +33,54 @@ def run_bot():
 
     @client.event
     async def on_message(message):
+        if message.author == client.user:
+            return
+
         if message.content.startswith('!go'):
             try:
-                voice_client = await message.author.voice.channel.connect()
-                voice_clients[voice_client.guild.id] = voice_client
+                if not message.author.voice:
+                    await message.channel.send("Você precisa estar em um canal de voz!")
+                    return
+
+                voice_channel = message.author.voice.channel
+                voice_client = discord.utils.get(client.voice_clients, guild=message.guild)
+                if not voice_client:
+                    voice_client = await voice_channel.connect()
+                    voice_clients[message.guild.id] = voice_client
+                elif voice_client.channel != voice_channel:
+                    await voice_client.move_to(voice_channel)
+
+                try:
+                    url = message.content.split()[1]
+                    loop = asyncio.get_event_loop()
+                    data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
+                    
+                    song = data['url']
+                    title = data.get('title', 'Desconhecido')
+
+                    player = discord.FFmpegPCMAudio(song, **ffmpeg_options)
+                    voice_clients[message.guild.id].play(player, after=lambda e: print(f"Terminou de tocar: {e}"))
+                    await message.channel.send(f"Tocando agora: **{title}**")
+
+                except IndexError:
+                    await message.channel.send("Por favor, forneça um URL após o comando! Exemplo: `!go https://youtube.com/...`")
+                except Exception as e:
+                    await message.channel.send(f"Erro ao reproduzir: {str(e)}")
+                    print(f"Erro no yt-dlp: {e}")
+
             except Exception as e:
-                print(e)
-            try:
-                url = message.content.split()[1]
+                await message.channel.send("Erro ao conectar ao canal de voz!")
+                print(f"Erro na conexão: {e}")
 
-                loop = asyncio.get_event_loop()
-                data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
+        if message.content.startswith('!stop'):
+            if message.guild.id in voice_clients:
+                voice_client = voice_clients[message.guild.id]
+                if voice_client.is_playing():
+                    voice_client.stop()
+                await voice_client.disconnect()
+                del voice_clients[message.guild.id]
+                await message.channel.send("Desconectado do canal de voz!")
+            else:
+                await message.channel.send("Não estou em nenhum canal de voz!")
 
-                song = data['url']
-
-                player = discord.FFmpegPCMAudio(song, **ffmpeg_options)
-                voice_clients[message.guild.id].play(player)
-
-            except Exception as e:
-                print(e)
-
-    client.run(TOKEN)      
+    client.run(TOKEN)
